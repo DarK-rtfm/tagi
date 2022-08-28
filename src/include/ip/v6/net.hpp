4e6ac5c16e9e5777cf229fc08723d3706ec1bfa2
DarK-rtfm <44683230+DarK-rtfm@users.noreply.github.com>
2022-08-28 19:40:25 +0200

    ipv6 specialization skeleton

---
+++ src/include/ip/v6/net.hpp
@@ -0,0 +1,13 @@
+#ifndef __NET_6_HH
+#define __NET_6_HH
+
+#include "ip/v6/ip.hpp"
+#include "ip/v6/mask.hpp"
+#include "ip/base/net_t.hpp"
+
+namespace IP
+{
+    using NET6 = NET<__uint128_t>;
+}
+
+#endif
---
+++ src/lib/ip/v6/ip.cc
@@ -0,0 +1,16 @@
+#include "ip/v6/ip.hpp"
+#include "ip/base/ip_t.hpp"
+#include <iomanip>
+
+namespace IP
+{
+    template <>
+    IP6::IP(std::string str)
+    {
+    }
+
+    template <>
+    std::string IP6::toString(char _flags)
+    {
+    }
+}
